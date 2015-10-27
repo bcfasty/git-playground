69634e267902ea61adb27ecb207f2171e950e425
Sven Buchberger <sven.buchberger@rhd.de>
2015-10-27 08:19:32 +0100

    Typo Fix

--- index.php
+++ index.php
@@ -6,4 +6,4 @@ require "lib/Car.php";
 
 $car = new Car();
 $car->drive(20); // 50 is to fast :) 30 is alsoy too fast
-echo "Car has driven " . $car->getKm() . " km";
+echo "Car has driven " . $car->getKm() . " km.";
