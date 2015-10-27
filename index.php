54e13885913942606b810b0db5de108f3da103d3
Sven Buchberger <sven.buchberger@rhd.de>
2015-10-27 08:18:54 +0100

    car drives 20

--- index.php
+++ index.php
@@ -5,5 +5,5 @@
 require "lib/Car.php";
 
 $car = new Car();
-$car->drive(30); // 50 is to fast :)
+$car->drive(20); // 50 is to fast :) 30 is alsoy too fast
 echo "Car has driven " . $car->getKm() . " km";
